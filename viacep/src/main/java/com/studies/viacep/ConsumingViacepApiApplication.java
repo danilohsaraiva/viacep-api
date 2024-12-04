293ec73c8ac3daae3ff1e170c94343409d62e4ce
Danilo <danilohsaraiva@gmail.com>
2024-12-04 17:10:12 -0300

    Subindo projeto

---
+++ viacep/src/main/java/com/studies/viacep/ConsumingViacepApiApplication.java
@@ -0,0 +1,13 @@
+package com.studies.viacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingViacepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingViacepApiApplication.class, args);
+	}
+
+}
---
+++ viacep/src/test/java/com/studies/viacep/ConsumingViacepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.viacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingViacepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
